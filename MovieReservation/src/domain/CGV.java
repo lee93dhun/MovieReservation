38cc9b14c9f706f909a7aa6b23182ed58fca09cd
lee93dhun <dohuny2@naver.com>
2021-06-14 14:58:16 +0900

    z

--- MovieReservation/src/domain/CGV.java
+++ MovieReservation/src/domain/CGV.java
@@ -1,13 +1,16 @@
-package main;
+package domain;
 
-public class 롯데시네마 extends 영화관 {
-	int 고유번호 = 03;
+public class CGV extends 영화관 {
+	int 고유번호 = 02;
 	int 금액;
+
+	public CGV() {
+	}
 
 	@Override
 	public void 메뉴() {
 		while (true) {
-			System.out.println("롯데시네마 메뉴");
+			System.out.println("CGV 메뉴");
 			System.out.println("1.로그인 2.회원가입 3. 예매 4. 종료");
 			int 선택 = sc.nextInt();
 			if (선택 == 1) {
@@ -19,34 +22,35 @@ public class 롯데시네마 extends 영화관 {
 			if (선택 == 3) {
 				예매();
 			}
-			
 			if (선택 == 4) {
 				종료();
 				break;
 			}
 		}
+
 	}
 
 	@Override
 	public void 예매() {
-
+		
 		super.예매();
 	}
 
 	@Override
 	public void 로그인() {
-
+	
 		super.로그인();
 	}
 
 	@Override
 	public void 회원가입() {
-
+	
 		super.회원가입();
 	}
 
 	@Override
 	public void 종료() {
+	
 		super.종료();
 	}
 
