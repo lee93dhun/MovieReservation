939df3393296200da9ec53ec8875d2a123329b64
lee93dhun <dohuny2@naver.com>
2021-06-14 15:51:16 +0900

    1

--- MovieReservation/src/controller/영화예매.java
+++ MovieReservation/src/controller/영화예매.java
@@ -2,6 +2,10 @@ package controller;
 
 import java.util.Scanner;
 
+import domain.영화회원;
+import domain.키오스크;
+
+
 public class 영화예매 {
 	
 	
@@ -14,9 +18,10 @@ public class 영화예매 {
 		System.out.println("==========영화예매프로그램==========");
 		System.out.println("1.회원가입     2. 로그인      3.종료");
 		
+		키오스크 키오스크 = new 영화회원();
 		int 선택 = scanner.nextInt();
-		if( 선택 == 1) { }
-		if( 선택 == 2) { }
+		if( 선택 == 1) { 키오스크.회원가입(); }
+		if( 선택 == 2) { 키오스크.로그인();}
 		if( 선택 == 3) { System.out.println("------> 예매 프로그램 종료");}
 			
 		
--- MovieReservation/src/domain/list.java
+++ MovieReservation/src/domain/list.java
@@ -2,7 +2,7 @@ package domain;
 
 import java.util.ArrayList;
 
-public class 영화회원목록 {
+public class list {
 	public static ArrayList<영화회원>영화회원목록 = new ArrayList<>();
 
 }
