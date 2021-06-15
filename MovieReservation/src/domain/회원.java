63dde786e01ce24529128662cb3fc83c303be737
lee93dhun <85606299+lee93dhun@users.noreply.github.com>
2021-06-15 13:33:21 +0900

    Update 회원.java

--- MovieReservation/src/domain/회원.java
+++ MovieReservation/src/domain/회원.java
@@ -1,7 +1,8 @@
 package domain;
 
+import controller.ì˜ˆë§¤í”„ë¡œê·¸ë¨;
 
-public class È¸¿ø implements Å°¿À½ºÅ© {
+public class íšŒì› implements í‚¤ì˜¤ìŠ¤í¬ {
 	
 	
 	
@@ -13,10 +14,10 @@ public class È¸¿ø implements Å°¿À½ºÅ© {
 	
 		
 	
-	// ºó »ı¼ºÀÚ
-	public È¸¿ø() { }
-	// ¸ğµç ÇÊµå »ı¼ºÀÚ
-	public È¸¿ø(String id, String password, String name) {
+	// ë¹ˆ ìƒì„±ì
+	public íšŒì›() { }
+	// ëª¨ë“  í•„ë“œ ìƒì„±ì
+	public íšŒì›(String id, String password, String name) {
 		super();
 		this.id = id;
 		this.password = password;
@@ -24,31 +25,53 @@ public class È¸¿ø implements Å°¿À½ºÅ© {
 	}
 	
 	@Override
-	public void ¸Ş´º() {
+	public void ë©”ë‰´() {
 	}
 
 
 	@Override
-	public void ·Î±×ÀÎ() {
-		
-//		System.out.println("ID¸¦ ÀÔ·ÂÇÏ¼¼¿ä : ");String id	 = ¿¹¸ÅÇÁ·Î±×·¥.scanner.next();
-//		System.out.println("Password¸¦ ÀÔ·ÂÇÏ¼¼¿ä : "); String password	 = ¿¹¸ÅÇÁ·Î±×·¥.scanner.next();
-//			for( int i = 0; i<list.¿µÈ­È¸¿ø¸ñ·Ï.size(); i++ ) {
-//				if( list.¿µÈ­È¸¿ø¸ñ·Ï.get(i).id.equals(id) && list.¿µÈ­È¸¿ø¸ñ·Ï.get(i).equals(password) ) {
-//					System.out.println("·Î±×ÀÎ ¼º°ø ---------------->");
-//				}
-//				else { System.out.println("ÀÔ·ÂÇÏ½Å Á¤º¸°¡ ÀÏÄ¡ÇÏÁö ¾Ê½À´Ï´Ù.");
-//				}
-//			}
+	public void ë¡œê·¸ì¸() {
+		System.out.println("IDë¥¼ ì…ë ¥í•˜ì„¸ìš” : ");String id	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+		System.out.println("Passwordë¥¼ ì…ë ¥í•˜ì„¸ìš” : "); String password	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+			for( int i = 0; i<list.ì˜í™”íšŒì›ëª©ë¡.size(); i++ ) {
+				if( list.ì˜í™”íšŒì›ëª©ë¡.get(i).equals(id) && list.ì˜í™”íšŒì›ëª©ë¡.get(i).equals(password) ) {
+					System.out.println("ë¡œê·¸ì¸ ì„±ê³µ ---------------->");
+				}
+				else { System.out.println("ì…ë ¥í•˜ì‹  ì •ë³´ê°€ ì¼ì¹˜í•˜ì§€ ì•ŠìŠµë‹ˆë‹¤.");
+				}
+			}
 		
 	}
 
 	@Override
-	public void È¸¿ø°¡ÀÔ() {
+	public void íšŒì›ê°€ì…() {
+		int ìŠ¤ìœ„ì¹˜ =0 ;
+		System.out.println("IDë¥¼ ì…ë ¥í•˜ì„¸ìš” : ");String id	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+	while( true ) {
+		for(int i = 0 ; i<list.ì˜í™”íšŒì›ëª©ë¡.size(); i++) {
+									//ë¦¬ìŠ¤íŠ¸ë‚´ ê°ì²´ ìˆ˜
+			if(list.ì˜í™”íšŒì›ëª©ë¡.get(i).equals(id)) {
+							// ië²ˆì§¸ì˜ ì•„ì´ë””ê°€ í˜„ì¬ìœ„ì—ì…ë ¥ëœ ì•„ì´ë””ì™€ ë™ì¼í•˜ë©´
+				System.out.println("------>í˜„ì¬ ì‚¬ìš©ì¤‘ì¸ ì•„ì´ë””ì…ë‹ˆë‹¤.");
+				ìŠ¤ìœ„ì¹˜ = 1 ;
+				break;
+			}
+		}
+		if(ìŠ¤ìœ„ì¹˜ == 0) {
+			System.out.println("Passwordë¥¼ ì…ë ¥í•˜ì„¸ìš” : "); String password	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+			System.out.println("ì´ë¦„ì„ ì…ë ¥í•˜ì„¸ìš” : "); String name	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+			
+			íšŒì› temp = new íšŒì›(id, password, name );
+			list.ì˜í™”íšŒì›ëª©ë¡.add(temp);
+			System.out.println("-------> íšŒì›ê°€ì… ì„±ê³µ");
+			return;
+		}
+	}
+	
 		
 	}
 	@Override
-	public void ºñÈ¸¿ø¿¹¸Å() {
+	public void ë¹„íšŒì›ì˜ˆë§¤() {
 	}
 
 	
@@ -89,3 +112,4 @@ public class È¸¿ø implements Å°¿À½ºÅ© {
 	
 	
 }	// class end
+
