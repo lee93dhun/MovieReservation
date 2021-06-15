b3ef8318d3556cecbdb308250fd2b12616498582
dfafw12 <78467904+dfafw12@users.noreply.github.com>
2021-06-15 15:57:22 +0900

    Update 롯데시네마.java

--- MovieReservation/src/domain/롯데시네마.java
+++ MovieReservation/src/domain/롯데시네마.java
@@ -1,19 +1,22 @@
 package domain;
 
-import java.util.Scanner;
-
 import controller.예매프로그램;
 
-public class 롯데시네마 extends 극장 {
-	int 고유번호 = 03;
+public class 메가박스 extends 극장 {
+
+	int 고유번호 = 01;
 	int 금액;
+	회원 회원 = new 회원();
+
+	public 메가박스() {
+	}
+
+	public 메가박스(String id, String password, String name) {
+	}
 
 	@Override
 	public void 메뉴() {
-		while (true) {
-			System.out.println("롯데시네마 메뉴");
-			super.메뉴();
-		}
+		super.메뉴();
 	}
 
 	@Override
@@ -23,18 +26,21 @@ public class 롯데시네마 extends 극장 {
 
 	@Override
 	public void 로그인() {
+		System.out.println("===========메가박스 로그인===========");
+		회원.로그인();
 
-		super.로그인();
 	}
 
 	@Override
 	public void 회원가입() {
-
-		super.회원가입();
+		System.out.println("==========메가박스 회원가입==========");
+		회원.회원가입();
 	}
 
 	@Override
 	public void 종료() {
+
 		super.종료();
 	}
+
 }
