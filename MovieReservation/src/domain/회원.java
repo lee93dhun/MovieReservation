8031752666c5b5ec0ec07e9605e05218c8aa7c54
lee93 <lee93@DESKTOP-GMN3K6E>
2021-06-15 13:29:54 +0900

    1

--- MovieReservation/src/domain/회원.java
+++ MovieReservation/src/domain/회원.java
@@ -1,5 +1,6 @@
 package domain;
 
+import controller.예매프로그램;
 
 public class 회원 implements 키오스크 {
 	
@@ -30,21 +31,43 @@ public class 회원 implements 키오스크 {
 
 	@Override
 	public void 로그인() {
-		
-//		System.out.println("ID를 입력하세요 : ");String id	 = 예매프로그램.scanner.next();
-//		System.out.println("Password를 입력하세요 : "); String password	 = 예매프로그램.scanner.next();
-//			for( int i = 0; i<list.영화회원목록.size(); i++ ) {
-//				if( list.영화회원목록.get(i).id.equals(id) && list.영화회원목록.get(i).equals(password) ) {
-//					System.out.println("로그인 성공 ---------------->");
-//				}
-//				else { System.out.println("입력하신 정보가 일치하지 않습니다.");
-//				}
-//			}
+		System.out.println("ID를 입력하세요 : ");String id	 = 예매프로그램.scanner.next();
+		System.out.println("Password를 입력하세요 : "); String password	 = 예매프로그램.scanner.next();
+			for( int i = 0; i<list.영화회원목록.size(); i++ ) {
+				if( list.영화회원목록.get(i).equals(id) && list.영화회원목록.get(i).equals(password) ) {
+					System.out.println("로그인 성공 ---------------->");
+				}
+				else { System.out.println("입력하신 정보가 일치하지 않습니다.");
+				}
+			}
 		
 	}
 
 	@Override
 	public void 회원가입() {
+		int 스위치 =0 ;
+		System.out.println("ID를 입력하세요 : ");String id	 = 예매프로그램.scanner.next();
+	while( true ) {
+		for(int i = 0 ; i<list.영화회원목록.size(); i++) {
+									//리스트내 객체 수
+			if(list.영화회원목록.get(i).equals(id)) {
+							// i번째의 아이디가 현재위에입력된 아이디와 동일하면
+				System.out.println("------>현재 사용중인 아이디입니다.");
+				스위치 = 1 ;
+				break;
+			}
+		}
+		if(스위치 == 0) {
+			System.out.println("Password를 입력하세요 : "); String password	 = 예매프로그램.scanner.next();
+			System.out.println("이름을 입력하세요 : "); String name	 = 예매프로그램.scanner.next();
+			
+			회원 temp = new 회원(id, password, name );
+			list.영화회원목록.add(temp);
+			System.out.println("-------> 회원가입 성공");
+			return;
+		}
+	}
+	
 		
 	}
 	@Override
