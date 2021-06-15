ceabfad09adb38ff8c8a4f77d6912486a14e8ecf
dfafw12 <78467904+dfafw12@users.noreply.github.com>
2021-06-15 15:58:21 +0900

    Update 영화.java

--- MovieReservation/src/domain/영화.java
+++ MovieReservation/src/domain/영화.java
@@ -1,46 +1,48 @@
+package domain;
+
 public class 영화 {
 	String 제목;
 	String 장르;
 	int 영화번호;
 	int 금액;
-	
-	
-	
-	public 영화() {}
-	
-	public 영화(String 제목, int 영화번호,int 금액) {
+
+	public 영화() {
+	}
+
+	public 영화(String 제목, int 영화번호, int 금액) {
 		this.제목 = 제목;
-		
+
 		this.영화번호 = 영화번호;
 		this.금액 = 금액;
-		
+
 	}
-	public void 영화1() {
-		제목 = "ㅇㅇ";
+
+	public void 크루엘라() {
+		제목 = "크루엘라";
 		영화번호 = 11;
 		금액 = 7000;
-		
+
 		영화 temp = new 영화(제목, 영화번호, 금액);
 		list.영화목록.add(temp);
-		
-		
+
 	}
-	public void 영화2() {
-		제목 = "ㄴㄴ";
+
+	public void 컨저링3() {
+		제목 = "컨저링3";
 		영화번호 = 22;
 		금액 = 8000;
 		영화 temp = new 영화(제목, 영화번호, 금액);
 		list.영화목록.add(temp);
-		
+
 	}
-	public void 영화3() {
-		제목 = "ㅁㅁ";
+
+	public void 캐시트럭() {
+		제목 = "캐시트럭";
 		영화번호 = 33;
 		금액 = 9000;
 		영화 temp = new 영화(제목, 영화번호, 금액);
 		list.영화목록.add(temp);
 
 	}
-
 
 }
