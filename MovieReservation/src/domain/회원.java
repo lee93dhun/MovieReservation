0ae9d039bb4938275880dcc84cd67261c3196332
dfafw12 <78467904+dfafw12@users.noreply.github.com>
2021-06-15 14:51:39 +0900

    Update 회원.java

--- MovieReservation/src/domain/회원.java
+++ MovieReservation/src/domain/회원.java
@@ -1,8 +1,8 @@
 package domain;
 
-import controller.¿¹¸ÅÇÁ·Î±×·¥;
+import controller.ì˜ˆë§¤í”„ë¡œê·¸ë¨;
 
-public class È¸¿ø implements Å°¿À½ºÅ© {
+public class íšŒì› implements í‚¤ì˜¤ìŠ¤í¬ {
 	
 	
 	
@@ -10,71 +10,92 @@ public class È¸¿ø implements Å°¿À½ºÅ© {
 	private String id;
 	private String password;
 	private String name;
+	int ê³ ìœ ë²ˆí˜¸;
 		
 	
 		
 	
-	// ºó »ı¼ºÀÚ
-	public È¸¿ø() { }
-	// ¸ğµç ÇÊµå »ı¼ºÀÚ
-	public È¸¿ø(String id, String password, String name) {
+	// ë¹ˆ ìƒì„±ì
+	public íšŒì›() { }
+	// ëª¨ë“  í•„ë“œ ìƒì„±ì
+	public íšŒì›(String id, String password, String name, int ê³ ìœ ë²ˆí˜¸) {
 		super();
 		this.id = id;
 		this.password = password;
 		this.name = name;
+		this.ê³ ìœ ë²ˆí˜¸ = ê³ ìœ ë²ˆí˜¸;
 	}
 	
 	@Override
-	public void ¸Ş´º() {
+	public void ë©”ë‰´() {
 	}
 
 
 	@Override
-	public void ·Î±×ÀÎ() {
-		System.out.println("ID¸¦ ÀÔ·ÂÇÏ¼¼¿ä : ");String id	 = ¿¹¸ÅÇÁ·Î±×·¥.scanner.next();
-		System.out.println("Password¸¦ ÀÔ·ÂÇÏ¼¼¿ä : "); String password	 = ¿¹¸ÅÇÁ·Î±×·¥.scanner.next();
-			for( int i = 0; i<list.¿µÈ­È¸¿ø¸ñ·Ï.size(); i++ ) {
-				if( list.¿µÈ­È¸¿ø¸ñ·Ï.get(i).id.equals(id) && list.¿µÈ­È¸¿ø¸ñ·Ï.get(i).password.equals(password) ) {
-					System.out.println("·Î±×ÀÎ ¼º°ø ---------------->");
+	public void ë¡œê·¸ì¸() {
+		System.out.println("IDë¥¼ ì…ë ¥í•˜ì„¸ìš” : ");String id	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+		System.out.println("Passwordë¥¼ ì…ë ¥í•˜ì„¸ìš” : "); String password	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+			for( int i = 0; i<list.ì˜í™”íšŒì›ëª©ë¡.size(); i++ ) {
+				if( list.ì˜í™”íšŒì›ëª©ë¡.get(i).id.equals(id) && list.ì˜í™”íšŒì›ëª©ë¡.get(i).password.equals(password) ) {
+					System.out.println("ë¡œê·¸ì¸ ì„±ê³µ ---------------->");
+					System.out.println("1.ì˜ˆë§¤ 2. ì˜ˆë§¤ì·¨ì†Œ 3. ì¢…ë£Œ");
+					int ì„ íƒ = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.nextInt();
+					if(ì„ íƒ ==1) {
+						ì˜ˆë§¤();
+					}
+					if(ì„ íƒ ==2) {
+						
+					}
+					if(ì„ íƒ ==3) {}
 				}
-				else { System.out.println("ÀÔ·ÂÇÏ½Å Á¤º¸°¡ ÀÏÄ¡ÇÏÁö ¾Ê½À´Ï´Ù.");
+				else { System.out.println("ì…ë ¥í•˜ì‹  ì •ë³´ê°€ ì¼ì¹˜í•˜ì§€ ì•ŠìŠµë‹ˆë‹¤.");
 				}
+				
 			}
 		
 	}
 
 	@Override
-	public void È¸¿ø°¡ÀÔ() {
+	public void íšŒì›ê°€ì…() {
 		
 		
 	while( true ) {
-		int ½ºÀ§Ä¡ =0 ;
-		System.out.println("ID¸¦ ÀÔ·ÂÇÏ¼¼¿ä : ");String id	 = ¿¹¸ÅÇÁ·Î±×·¥.scanner.next();
+		int ìŠ¤ìœ„ì¹˜ =0 ;
+		System.out.println("IDë¥¼ ì…ë ¥í•˜ì„¸ìš” : ");String id	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
 		
-		for(int i = 0 ; i<list.¿µÈ­È¸¿ø¸ñ·Ï.size(); i++) {
-									//¸®½ºÆ®³» °´Ã¼ ¼ö
-			if(list.¿µÈ­È¸¿ø¸ñ·Ï.get(i).id.equals(id)) {
-							// i¹øÂ°ÀÇ ¾ÆÀÌµğ°¡ ÇöÀçÀ§¿¡ÀÔ·ÂµÈ ¾ÆÀÌµğ¿Í µ¿ÀÏÇÏ¸é
-				System.out.println("------>ÇöÀç »ç¿ëÁßÀÎ ¾ÆÀÌµğÀÔ´Ï´Ù.");
-				½ºÀ§Ä¡ = 1 ;
+		for(int i = 0 ; i<list.ì˜í™”íšŒì›ëª©ë¡.size(); i++) {
+									//ë¦¬ìŠ¤íŠ¸ë‚´ ê°ì²´ ìˆ˜
+			if(list.ì˜í™”íšŒì›ëª©ë¡.get(i).id.equals(id)) {
+							// ië²ˆì§¸ì˜ ì•„ì´ë””ê°€ í˜„ì¬ìœ„ì—ì…ë ¥ëœ ì•„ì´ë””ì™€ ë™ì¼í•˜ë©´
+				System.out.println("------>í˜„ì¬ ì‚¬ìš©ì¤‘ì¸ ì•„ì´ë””ì…ë‹ˆë‹¤.");
+				ìŠ¤ìœ„ì¹˜ = 1 ;
 				break;
 			}
-		}
-		if(½ºÀ§Ä¡ == 0) {
-			System.out.println("Password¸¦ ÀÔ·ÂÇÏ¼¼¿ä : "); String password	 = ¿¹¸ÅÇÁ·Î±×·¥.scanner.next();
-			System.out.println("ÀÌ¸§À» ÀÔ·ÂÇÏ¼¼¿ä : "); String name	 = ¿¹¸ÅÇÁ·Î±×·¥.scanner.next();
 			
-			È¸¿ø temp = new È¸¿ø(id, password, name );
-			list.¿µÈ­È¸¿ø¸ñ·Ï.add(temp);
-			System.out.println("-------> È¸¿ø°¡ÀÔ ¼º°ø");
+		}
+		if(ìŠ¤ìœ„ì¹˜ == 0) {
+			System.out.println("Passwordë¥¼ ì…ë ¥í•˜ì„¸ìš” : "); String password	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+			System.out.println("ì´ë¦„ì„ ì…ë ¥í•˜ì„¸ìš” : "); String name	 = ì˜ˆë§¤í”„ë¡œê·¸ë¨.scanner.next();
+			
+			íšŒì› temp = new íšŒì›(id, password, name, ê³ ìœ ë²ˆí˜¸ );
+			list.ì˜í™”íšŒì›ëª©ë¡.add(temp);
+			
+			System.out.println("-------> íšŒì›ê°€ì… ì„±ê³µ");
 			return;
+			
 		}
 	}
 	
 		
 	}
 	@Override
-	public void ºñÈ¸¿ø¿¹¸Å() {
+	public void ë¹„íšŒì›ì˜ˆë§¤() {
+	}
+	
+	@Override
+	public void ì˜ˆë§¤() {
+		
+		
 	}
 
 	
