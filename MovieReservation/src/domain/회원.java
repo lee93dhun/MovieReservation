f8a87a925c5421967db06c34d88699ec17dae644
dfafw12 <78467904+dfafw12@users.noreply.github.com>
2021-06-16 14:03:40 +0900

    Update 회원.java

--- MovieReservation/src/domain/회원.java
+++ MovieReservation/src/domain/회원.java
@@ -2,94 +2,124 @@ package domain;
 
 import controller.예매프로그램;
 
-public class 극장 implements 키오스크 {
+public class 회원 implements 키오스크 {
 
-	// 필드
-	String 아이디;
-	String 비밀번호;
-	String 이름;
-	int 금액;
+	private String id;
+	private String password;
+	private String name;
+	int 고유번호;
 
 	// 빈 생성자
-	public 극장() {
+	public 회원() {
 	}
-	// 모든 필드 생성자
 
-	public 극장(String 아이디, String 비밀번호, String 이름, int 금액) {
+	// 모든 필드 생성자
+	public 회원(String id, String password, String name, int 고유번호) {
 		super();
-		this.아이디 = 아이디;
-		this.비밀번호 = 비밀번호;
-		this.이름 = 이름;
-		this.금액 = 금액;
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.고유번호 = 고유번호;
 	}
 
 	@Override
 	public void 메뉴() {
-
-		while (true) {
-
-			System.out.println("1.로그인 2.회원가입  3.비회원예매 4.종료");
-			int 선택 = 예매프로그램.scanner.nextInt();
-			if (선택 == 1) {
-				로그인();
-			}
-			if (선택 == 2) {
-				회원가입();
-			}
-
-			if (선택 == 3) {
-				비회원예매();
-			}
-			if (선택 == 4) {
-				return;
-			}
-		}
-
 	}
 
 	@Override
 	public void 로그인() {
+		System.out.println("ID를 입력하세요 : ");
+		String id = 예매프로그램.scanner.next();
+		System.out.println("Password를 입력하세요 : ");
+		String password = 예매프로그램.scanner.next();
+		for (int i = 0; i < list.영화회원목록.size(); i++) {
+			if (list.영화회원목록.get(i).id.equals(id) && list.영화회원목록.get(i).password.equals(password)) {
+				System.out.println("로그인 성공 ---------------->");
+				System.out.println("1.예매 2. 예매취소 3. 종료");
+				int 선택 = 예매프로그램.scanner.nextInt();
+				if (선택 == 1) {
+
+				}
+				if (선택 == 2) {
+
+				}
+				if (선택 == 3) {
+				}
+			} else {
+				System.out.println("입력하신 정보가 일치하지 않습니다.");
+			}
+
+		}
 
 	}
 
 	@Override
 	public void 회원가입() {
 
-	}
+		while (true) {
+			int 스위치 = 0;
+			System.out.println("ID를 입력하세요 : ");
+			String id = 예매프로그램.scanner.next();
 
-	@Override
-	public void 예매() {
+			for (int i = 0; i < list.영화회원목록.size(); i++) {
+				// 리스트내 객체 수
+				if (list.영화회원목록.get(i).id.equals(id)) {
+					// i번째의 아이디가 현재위에입력된 아이디와 동일하면
+					System.out.println("------>현재 사용중인 아이디입니다.");
+					스위치 = 1;
+					break;
+				}
+
+			}
+			if (스위치 == 0) {
+				System.out.println("Password를 입력하세요 : ");
+				String password = 예매프로그램.scanner.next();
+				System.out.println("이름을 입력하세요 : ");
+				String name = 예매프로그램.scanner.next();
+
+				회원 temp = new 회원(id, password, name, 고유번호);
+				list.영화회원목록.add(temp);
+
+				System.out.println("-------> 회원가입 성공");
+				return;
+
+			}
+		}
 
 	}
 
 	@Override
 	public void 비회원예매() {
-		System.out.println("영화 선택");
-		System.out.println("1.크루엘라, 2.컨저링3 3.캐시트럭");
-		int 선택 = 예매프로그램.scanner.nextInt();
-		if (선택 == 1) {
-			영화 temp = new 영화();
-			temp.크루엘라();
-			System.out.println(temp.제목 + " 좌석 : ");
-			
-			System.out.println(temp.제목 + " 금액 : " + temp.금액 + "원");
-			
-
-		}
-		if (선택 == 2) {
-			영화 temp = new 영화();
-			temp.컨저링3();
-			System.out.println(temp.제목 + " 좌석 : ");
-			
-			System.out.println(temp.제목 + " 금액 : " + temp.금액 + "원");
-		}
-		if (선택 == 3) {
-			영화 temp = new 영화();
-			temp.캐시트럭();
-			System.out.println(temp.제목 + " 좌석 : ");
-			
-			System.out.println(temp.제목 + " 금액 : " + temp.금액 + "원");
-		}
+	}
+	
+	@Override
+	public void 예매() {
+		
+		
 	}
 
-}
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+} // class end
