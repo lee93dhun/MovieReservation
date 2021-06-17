c21ac90171d39e046cef17da4f4cb900b0320afa
dfafw12 <78467904+dfafw12@users.noreply.github.com>
2021-06-17 17:01:55 +0900

    Update CGV.java

--- MovieReservation/src/domain/CGV.java
+++ MovieReservation/src/domain/CGV.java
@@ -1,52 +1,47 @@
 package domain;
 
-
 public class CGV extends 극장 {
+	String id, password, name;
 	int 고유번호 = 02;
 	int 금액;
+	회원 회원 = new 회원();
 
 	public CGV() {
 	}
 
+	public CGV(String id, String password, String name, int 금액, int 고유번호) {
+		super(id, password, name, 금액);
+		this.고유번호 = 고유번호;
+
+	}
+
 	@Override
 	public void 메뉴() {
-		while (true) {
-			System.out.println("CGV 메뉴");
-			super.메뉴();
-		}
+
+		System.out.println("CGV 메뉴");
+		super.메뉴();
+
 	}
 
 	@Override
 	public void 비회원예매() {
-
 		super.비회원예매();
 	}
 
 	@Override
 	public void 로그인() {
-
-		super.로그인();
+		System.out.println("===========CGV 로그인===========");
+		회원.로그인();
 	}
 
 	@Override
 	public void 회원가입() {
-
-		super.회원가입();
+		System.out.println("==========CGV 회원가입==========");
+		회원.회원가입();
 	}
 
 	@Override
 	public void 종료() {
-
 		super.종료();
 	}
-
-}
-
-
-	@Override
-	public void 종료() {
-
-		super.종료();
-	}
-
 }
