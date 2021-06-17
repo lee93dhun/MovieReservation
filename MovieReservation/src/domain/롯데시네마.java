c643c3192394337338b125737e068b9dc7cbfc2c
dfafw12 <78467904+dfafw12@users.noreply.github.com>
2021-06-17 17:01:36 +0900

    Update 롯데시네마.java

--- MovieReservation/src/domain/롯데시네마.java
+++ MovieReservation/src/domain/롯데시네마.java
@@ -1,21 +1,26 @@
 package domain;
 
 
-public class 롯데시네마 extends 극장 {
 
-	int 고유번호 = 01;
+
+public class 롯데시네마 extends 극장 {
+	String id,password,name;
+	int 고유번호 = 03;
 	int 금액;
 	회원 회원 = new 회원();
-
-	public 롯데시네마() {
+	public 롯데시네마() {}
+	
+	public 롯데시네마(String id, String password, String name,int 금액, int 고유번호) {
+		super(id,password,name,금액);
+		this.고유번호 = 고유번호;
 	}
-
-	public 롯데시네마(String id, String password, String name) {
-	}
-
+	
 	@Override
 	public void 메뉴() {
+
+		System.out.println("롯데시네마 메뉴");
 		super.메뉴();
+
 	}
 
 	@Override
@@ -27,7 +32,6 @@ public class 롯데시네마 extends 극장 {
 	public void 로그인() {
 		System.out.println("===========롯데시네마 로그인===========");
 		회원.로그인();
-
 	}
 
 	@Override
@@ -38,8 +42,6 @@ public class 롯데시네마 extends 극장 {
 
 	@Override
 	public void 종료() {
-
 		super.종료();
 	}
-
 }
