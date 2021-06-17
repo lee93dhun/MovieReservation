7a60f62c3be0d8b323f28134fb53843ac3e171cc
dfafw12 <78467904+dfafw12@users.noreply.github.com>
2021-06-17 17:04:11 +0900

    Update 극장.java

--- MovieReservation/src/domain/극장.java
+++ MovieReservation/src/domain/극장.java
@@ -1,5 +1,6 @@
 package domain;
 
+import controller.예매프로그램;
 
 public class 극장 implements 키오스크 {
 
@@ -63,32 +64,7 @@ public class 극장 implements 키오스크 {
 
 	@Override
 	public void 비회원예매() {
-		System.out.println("영화 선택");
-		System.out.println("1.크루엘라, 2.컨저링3 3.캐시트럭");
-		int 선택 = 예매프로그램.scanner.nextInt();
-		if (선택 == 1) {
-			영화 temp = new 영화();
-			temp.크루엘라();
-			System.out.println(temp.제목 + " 좌석 : ");
-			
-			System.out.println(temp.제목 + " 금액 : " + temp.금액 + "원");
-			
 
-		}
-		if (선택 == 2) {
-			영화 temp = new 영화();
-			temp.컨저링3();
-			System.out.println(temp.제목 + " 좌석 : ");
-			
-			System.out.println(temp.제목 + " 금액 : " + temp.금액 + "원");
-		}
-		if (선택 == 3) {
-			영화 temp = new 영화();
-			temp.캐시트럭();
-			System.out.println(temp.제목 + " 좌석 : ");
-			
-			System.out.println(temp.제목 + " 금액 : " + temp.금액 + "원");
-		}
 	}
 
 }
